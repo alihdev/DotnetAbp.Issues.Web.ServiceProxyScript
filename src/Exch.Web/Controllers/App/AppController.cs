b1ec5956163e788d38d3320761006985a7414703
Ali Hassan <97635301+alihdev@users.noreply.github.com>
2024-07-08 16:02:34 +0300

    Fix ReferenceError: Exch is not defined in ServiceProxyScript https://github.com/abpframework/abp/issues/20158#issuecomment-2213027024

---
+++ src/Exch.Web/Controllers/App/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Asp.Versioning;
+using Exch.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+
+namespace Exch.Web.Controllers.App;
+
+[Controller]
+[RemoteService(Name = "Exch")]
+[Area("Exch")]
+[ControllerName("Exch")]
+[Route("api/app/[controller]")]
+public class AppController : ExchController
+{
+}
