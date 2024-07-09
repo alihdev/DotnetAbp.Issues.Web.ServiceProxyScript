f730d9fecac9e9a7bd849d6123eea487cbcdc296
Ali Hassan <97635301+alihdev@users.noreply.github.com>
2024-07-09 11:10:26 +0300

    Refactor TransferController using AppProController

--- src/Exch.Web/Controllers/App/Transfers/TransferController.cs
+++ src/Exch.Web/Controllers/App/Transfers/TransferController.cs
@@ -1,43 +1,8 @@
 ﻿using Exch.App.Transfers;
 using Exch.App.Transfers.Dtos;
-using Exch.Web.Controllers.App;
-using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
-using Volo.Abp.Application.Dtos;
 
 namespace Exch.Web.Controllers.App.Transfers;
 
-public class TransferController(ITransferAppService transferAppService) : AppController
+public class TransferController(ITransferAppService transferAppService) : AppProController<TransferDto, int, TransferGetListInput, TransferCreateDto, TransferUpdateDto>(transferAppService)
 {
-    private readonly ITransferAppService _transferAppService = transferAppService;
-
-    [HttpPost]
-    public Task<TransferDto> CreateAsync(TransferCreateDto input)
-    {
-        return _transferAppService.CreateAsync(input);
-    }
-
-    [HttpPut("{id}")]
-    public Task<TransferDto> UpdateAsync(int id, TransferUpdateDto input)
-    {
-        return _transferAppService.UpdateAsync(id, input);
-    }
-
-    [HttpGet("{id}")]
-    public Task<TransferDto> GetAsync(int id)
-    {
-        return _transferAppService.GetAsync(id);
-    }
-
-    [HttpGet]
-    public Task<PagedResultDto<TransferDto>> GetListAsync(TransferGetListInput input)
-    {
-        return _transferAppService.GetListAsync(input);
-    }
-
-    [HttpDelete("{id}")]
-    public Task DeleteAsync(int id)
-    {
-        return _transferAppService.DeleteAsync(id);
-    }
 }
